b32a90a672a060615aecca664c6d49cf2efc7c84
atuldeshpande <adeshpande@jhu.edu>
2021-07-23 14:09:41 -0400

    Adding SCODE format files and restructuring the folder.

--- scripts/method_scripts/run_SCODE.sh
+++ scripts/method_scripts/run_SCODE.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script for executing SCODE
 # for data1
-ruby run_R.rb data1/expr.txt data2/ptime.txt out1 100 4 356 100 1000
+ruby run_R.rb data/data1_SCODE_expr.txt data/data1_SCODE_ptime.txt out1 100 4 356 100 1000
 
 # for data2
-ruby run_R.rb data2/expr.txt data2/ptime.txt out2 626 20 1886 100 1000
+ruby run_R.rb data/data2_SCODE_expr.txt data/data2_SCODE_ptime.txt out2 626 20 1886 100 1000
